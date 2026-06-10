d63fb1ed98c1b225a4d8cb3efdabb8556c58d38a
ddd_2025 <1097943091@qq.com>
2026-06-10 11:10:09 +0800

    Add files via upload

---
+++ val_2D.py
@@ -0,0 +1,128 @@
+import numpy as np
+import torch
+from medpy import metric
+from scipy.ndimage import zoom
+
+
+def calculate_metric_percase(pred, gt):
+    pred[pred > 0] = 1
+    gt[gt > 0] = 1
+    if pred.sum() > 0:
+        dice = metric.binary.dc(pred, gt)
+        hd95 = metric.binary.hd95(pred, gt)
+        return dice, hd95
+    else:
+        return 0, 0
+
+def test_single_volume(image, label, net, classes, patch_size=[256, 256]):
+    image, label = image.squeeze(0).cpu().detach().numpy(), label.squeeze(0).cpu().detach().numpy()
+    prediction = np.zeros_like(label)
+    for ind in range(image.shape[0]):
+        slice = image[ind, :, :] # 提取切片
+        x, y = slice.shape[0], slice.shape[1]
+        slice = zoom(slice, (patch_size[0] / x, patch_size[1] / y), order=0)
+        input = torch.from_numpy(slice).unsqueeze(0).unsqueeze(0).float().cuda()
+        net.eval()
+        with torch.no_grad():
+            if  isinstance(net(input),tuple):
+                res = net(input)
+            else:
+                res = net(input)
+            out = torch.argmax(torch.softmax(res, dim=1), dim=1).squeeze(0)
+            out = out.cpu().detach().numpy()
+            pred = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
+            prediction[ind] = pred
+
+    metric_list = []
+    for i in range(1, classes):
+        metric_list.append(calculate_metric_percase(prediction == i, label == i))
+    return metric_list
+
+def test_glas_volume(image, label, net, classes, patch_size=[256, 256]):
+    image, label = image.squeeze(0).cpu().detach().numpy(), label.squeeze(0).cpu().detach().numpy()
+    prediction = np.zeros_like(label)
+
+    slice = image
+    x, y = slice.shape[0], slice.shape[1]
+    slice = zoom(slice, (patch_size[0] / x, patch_size[1] / y), order=0)
+    input = torch.from_numpy(slice).unsqueeze(0).unsqueeze(0).float().cuda()
+    net.eval()
+    with torch.no_grad():
+        if  isinstance(net(input),tuple):
+            res = net(input)[0]
+        else:
+            res = net(input)
+        out = torch.argmax(torch.softmax(res, dim=1), dim=1).squeeze(0)
+        out = out.cpu().detach().numpy()
+        pred = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
+        prediction = pred
+
+    metric_list = []
+    for i in range(1, classes):
+        metric_list.append(calculate_metric_percase(prediction == i, label == i))
+    return metric_list
+
+
+def test_single_volume_ds(image, label, net, classes, patch_size=[256, 256]):
+    image, label = image.squeeze(0).cpu().detach().numpy(), label.squeeze(0).cpu().detach().numpy()
+    prediction = np.zeros_like(label)
+    for ind in range(image.shape[0]):
+        slice = image[ind, :, :]
+        x, y = slice.shape[0], slice.shape[1]
+        slice = zoom(slice, (patch_size[0] / x, patch_size[1] / y), order=0)
+        input = torch.from_numpy(slice).unsqueeze(0).unsqueeze(0).float().cuda()
+        net.eval()
+        with torch.no_grad():
+            output_main, _, _, _ = net(input)
+            out = torch.argmax(torch.softmax(
+                output_main, dim=1), dim=1).squeeze(0)
+            out = out.cpu().detach().numpy()
+            pred = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
+            prediction[ind] = pred
+    metric_list = []
+    for i in range(1, classes):
+        metric_list.append(calculate_metric_percase(prediction == i, label == i))
+    return metric_list
+
+
+
+
+
+def test_mix_volume(image, label, net, classes, patch_size=[256, 256]):
+    image, label = image.squeeze(0).cpu().detach(
+    ).numpy(), label.squeeze(0).cpu().detach().numpy()
+    prediction = np.zeros_like(label)
+    for ind in range(image.shape[0]):
+        slice = image[ind, :, :]
+        x, y = slice.shape[0], slice.shape[1]
+        slice = zoom(slice, (patch_size[0] / x, patch_size[1] / y), order=0)
+        
+        inputH, inputL = FFT_HL(slice)
+        # GaussianBlur
+        import cv2
+        imageB = cv2.GaussianBlur(slice, (15, 15), 0)  
+
+        inputL = torch.from_numpy(inputL).unsqueeze(0).unsqueeze(0).float().cuda()
+        inputH = torch.from_numpy(inputH).unsqueeze(0).unsqueeze(0).float().cuda()
+        inputB = torch.from_numpy(imageB).unsqueeze(0).unsqueeze(0).float().cuda()
+        
+        # input = torch.from_numpy(slice).unsqueeze(0).unsqueeze(0).float().cuda()
+
+        input = inputL
+        
+        net.eval()
+        with torch.no_grad():
+            if  isinstance(net(input),tuple):
+                res = net(input)[0]
+            else:
+                res = net(input)
+            out = torch.argmax(torch.softmax(res, dim=1), dim=1).squeeze(0)
+            out = out.cpu().detach().numpy()
+            pred = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
+            prediction[ind] = pred
+    metric_list = []
+    for i in range(1, classes):
+        metric_list.append(calculate_metric_percase(
+            prediction == i, label == i))
+    return metric_list
+
